59aae4d66b7c65b424ab76e55b09a09bd6b829e4
NamDo8467 <huynhnamdo@gmail.com>
2022-01-09 22:19:41 -0500

    add logic to check for winner

--- main/src/Square.jsx
+++ main/src/Square.jsx
@@ -3,20 +3,103 @@ import { PlayerTurnContext } from "./PlayerTurnContext";
 
 function Square(props) {
   const turn = useContext(PlayerTurnContext);
+  
+  const winnerAnnouncement = document.querySelector('.winner-announcement');
+  const checkRow = (squares) => {
+    if (squares[0].textContent == squares[1].textContent && squares[1].textContent == squares[2].textContent) {
+      if (squares[0].textContent != "" && squares[1].textContent != "" && squares[2].textContent != "") {
+        winnerAnnouncement.append(`${squares[0].textContent} wins`)
+        return true
+        
+      }
+        
+    } 
+    if (squares[3].textContent == squares[4].textContent && squares[4].textContent == squares[5].textContent) {
+      if (squares[3].textContent != "" && squares[4].textContent != "" && squares[5].textContent != "") {
+        winnerAnnouncement.append(`${squares[3].textContent} wins`)
+        return true
+      } 
+      
+    }
+    if (squares[6].textContent == squares[7].textContent && squares[7].textContent == squares[8].textContent) {
+      if (squares[6].textContent != "" && squares[7].textContent != "" && squares[8].textContent != "") {
+        winnerAnnouncement.append(`${squares[6].textContent} wins`)
+        return true
+      }
+    }
+  }
+  const checkColumn = (squares) => {
+    if (squares[0].textContent == squares[3].textContent && squares[3].textContent == squares[6].textContent) {
+      if (squares[0].textContent != "" && squares[3].textContent != "" && squares[6].textContent != "") {
+        winnerAnnouncement.append(`${squares[0].textContent} wins`)
+        return true
+        
+      }
+        
+    } 
+    if (squares[1].textContent == squares[4].textContent && squares[4].textContent == squares[7].textContent) {
+      if (squares[1].textContent != "" && squares[4].textContent != "" && squares[7].textContent != "") {
+        winnerAnnouncement.append(`${squares[1].textContent} wins`)
+        return true
+      } 
+      
+    }
+    if (squares[2].textContent == squares[5].textContent && squares[5].textContent == squares[8].textContent) {
+      if (squares[2].textContent != "" && squares[5].textContent != "" && squares[8].textContent != "") {
+        winnerAnnouncement.append(`${squares[2].textContent} wins`)
+        return true
+      }
+    }
+  }
+
+  const checkDiagonal = (squares) => {
+    if (squares[0].textContent == squares[4].textContent && squares[4].textContent == squares[8].textContent) {
+      if (squares[0].textContent != "" && squares[4].textContent != "" && squares[8].textContent != "") {
+        winnerAnnouncement.append(`${squares[0].textContent} wins`)
+        return true
+        
+      }
+        
+    } 
+    if (squares[2].textContent == squares[4].textContent && squares[4].textContent == squares[6].textContent) {
+      if (squares[2].textContent != "" && squares[4].textContent != "" && squares[6].textContent != "") {
+        winnerAnnouncement.append(`${squares[2].textContent} wins`)
+        return true
+      } 
+      
+    }
+  }
   const handleClick = () => {
     const squares = document.querySelectorAll(".square");
     const position = `${props.position}`;
+    
     for (let i = 0; i < squares.length; i++) {
       if (position == i && squares[i].textContent == "") {
-        squares[i].innerHTML = `<p style = margin-top:15px>${turn.playerTurn}</p>`;
+        if (turn.playerTurn == 'O') {
+          squares[i].innerHTML = `<p style = margin-top:15px;color:red;font-weight:bold>${turn.playerTurn}</p>`;
+        } else {
+          squares[i].innerHTML = `<p style = margin-top:15px>${turn.playerTurn}</p>`;
+        
+        }
+        
+        if (checkRow(squares) || checkColumn(squares) || checkDiagonal(squares)) {
+          squares.forEach(square => {
+            square.style.pointerEvents = "none";
+          });
+          return
+        }
+        
         if (turn.playerTurn == "X") {
           turn.setPlayerTurn("O");
         } else {
           turn.setPlayerTurn("X");
         }
+        
         break;
       }
     }
+    
+    
   };
   const style = {
     height: "50px",
