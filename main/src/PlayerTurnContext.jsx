6abc25ca2c077f8dea6fc1b39f7d3b41322b5b88
NamDo8467 <huynhnamdo@gmail.com>
2022-01-09 22:20:15 -0500

    delete unused function

--- main/src/PlayerTurnContext.jsx
+++ main/src/PlayerTurnContext.jsx
@@ -3,9 +3,6 @@ import React, { useState } from "react";
 export const PlayerTurnContext = React.createContext("X");
 export function PlayerTurnProvider(props) {
   const [playerTurn, setPlayerTurn] = useState("X");
-  const changeTurn = () => {
-    setPlayerTurn("O");
-  };
   return (
     <PlayerTurnContext.Provider value={{ playerTurn, setPlayerTurn }}>
       {props.children}
