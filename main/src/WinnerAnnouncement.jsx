4c543155ebfd2703eb0ec7f6a5a7afa5850a92b7
NamDo8467 <huynhnamdo@gmail.com>
2022-01-09 22:19:08 -0500

    add announcement when there is a winner

---
+++ main/src/WinnerAnnouncement.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function WinnerAnnouncement() {
+    const style = {
+        marginTop: "10px",
+      };
+    
+    return (
+        <div style={style}>
+            <p className="winner-announcement"></p>
+        </div>
+    )
+}
+
+export default WinnerAnnouncement
